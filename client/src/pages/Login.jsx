109d159df1625e63dae54cfff5f00666eb3011bb
HoaFEDeveloper <chauthanhhoa226@gmail.com>
2023-03-30 16:56:54 +0700

    React Context Login and Logout

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -9,6 +9,8 @@ const Login = () => {
   });
 
   const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
   const [error, setError] = useState(null);
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -17,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/auth/login", inputs);
+      await login(inputs);
       navigate("/");
     } catch (err) {
       setError(err.response.data);
