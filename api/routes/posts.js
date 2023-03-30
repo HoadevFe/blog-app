c2a8c26e931ad5f6573ec2516f0b4c67ce402e18
HoaFEDeveloper <chauthanhhoa226@gmail.com>
2023-03-30 21:49:33 +0700

    get and delete post and author on bouth client api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,17 @@
 import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePosts,
+} from "../controllers/post.js";
 const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePosts);
 
 export default router;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,57 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const { currentUser } = useContext(AuthContext);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchApi();
+  }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/280757017_2038704299645190_7931972686930481782_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_ohc=nRkPaKMf__IAX9qOXeG&_nc_ht=scontent.fdad3-1.fna&oh=00_AfAmObneEb-khq_G-ftMmkQysgk7Bs-rm_n-kJER5DCzBA&oe=6422A62E"
-            alt=""
-          />
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
-            <span>CTH</span>
-            <p>posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias
-          natus facilis provident eveniet ratione magni aliquid vitae eos quis
-          sapiente doloribus, totam odit recusandae dolore suscipit atque aut
-          sint.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, velit
-          delectus accusantium qui aut itaque facere explicabo, voluptate, ut
-          atque iusto aliquid consequatur sed! Eaque non ea dolores eveniet
-          earum.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, velit
-          delectus accusantium qui aut itaque facere explicabo, voluptate, ut
-          atque iusto aliquid consequatur sed! Eaque non ea dolores eveniet
-          earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
-          velit delectus accusantium qui aut itaque facere explicabo, voluptate,
-          ut atque iusto aliquid consequatur sed! Eaque non ea dolores eveniet
-          earum.
-        </p>
+        <h1>{post.title}</h1>
+        <p>{post.description}</p>
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   );
 };
